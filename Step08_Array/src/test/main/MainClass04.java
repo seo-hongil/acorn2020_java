e066b80159130a9ebafe30127b8210f05668fe32
hongil <shi8295@naver.com>
2020-06-02 17:29:24 +0900

    wrapper,array,

---
+++ Step08_Array/src/test/main/MainClass04.java
@@ -0,0 +1,28 @@
+package test.main;
+
+import test.mypac.Bike;
+
+public class MainClass04 {
+	public static void main(String[] args) {
+		/*
+		 * 1. Bike 객체를 담을 수 있는 방 3개 짜리 배열객체를 생성해서 참조값을 
+		 *    bike 라는 지역 변수에 담아 보세요.
+		 * 2. 배열의 각각의 방(0,1,2번) 에 Bike 객체를 생성해서 담아보세요.   
+		 * 3. 반복문 for 를 이용해서 배열의 각각의 방에 있는 Bike 객체의
+		 *    ride() 메소드를 호출해 보세요.
+		 */
+		
+		Bike[] bike= new Bike[3];
+		bike[0]=new Bike();
+		bike[1]=new Bike();
+		bike[2]=new Bike();
+		
+		for(int i=0; i<bike.length; i++) {
+		//i번째 방에 있는 Bike 객체의 참조값 불러오기
+		Bike tmp = bike[i];
+		//Bike 객체의 메소드 호출하기
+		tmp.ride();
+	
+		}
+	}
+}
