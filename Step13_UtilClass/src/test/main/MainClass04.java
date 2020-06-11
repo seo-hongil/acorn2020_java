336888d4bc4c1c67849d98757d01d72a0f066f3f
hongil <shi8295@naver.com>
2020-06-11 17:45:58 +0900

    Step13_utilClass added

---
+++ Step13_UtilClass/src/test/main/MainClass04.java
@@ -0,0 +1,32 @@
+package test.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import test.mypac.Car;
+
+public class MainClass04 {
+	public static void main(String[] args) {
+	// 1. Car type 을 저장할 수 있는 ArrayList 객체를 생성해서
+	// 참조값 List 인터페이스 type 지역변수 cars 에 담아 보세요.
+			
+	// 2. Car 객체(3개)를 생성해서 List 객체에 저장해 보세요.
+			
+			List<Car> cars=new ArrayList<>();
+			
+			cars.add(new Car("소나타"));
+			cars.add(new Car("프라이드"));
+			Car c1=new Car("그랜저");
+			cars.add(c1);
+	// 3. 반복문 for문을 이용해서 List 객체에 저장된 모든 Car 객체의 drive() 메소드를
+	// 순서대로 호출해 보세요.
+			for(int i=0; i<cars.size();i++) {
+				Car c=cars.get(i);
+				c.drive();
+			}
+			System.out.println("확장 for문을 열어요");
+			for(Car c:cars) {
+				c.drive();
+			}
+	}
+}
---
+++ Step13_UtilClass/src/test/mypac/Car.java
@@ -0,0 +1,16 @@
+package test.mypac;
+
+public class Car {
+	//필드
+	private String name;
+	//생성자
+	public Car(String name) {
+		this.name=name;
+	}
+	
+	public void drive() {
+		System.out.println("부릉부릉 달려요!");
+
+	}
+
+}
